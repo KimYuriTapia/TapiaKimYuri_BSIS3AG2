76811e1ceaa18774d3e75ce4d99c07377c0d3934
KimYuriTapia <tapia.kimyuri.bsis@gmail.com>
2026-02-24 11:08:38 +0800

    Add files via upload

---
+++ Tapia/TapiaKimYuri_MultiplyTwoNumbers.py
@@ -0,0 +1,10 @@
+
+print("Kim Yuri L. Tapia | BSIS 3AG2")
+print("Multiply Two Numbers")
+print("=========================")
+
+Kim = int(input("Enter the first number: "))
+Tapia = int(input("Enter the second number: "))
+
+
+print("Result: ", Kim, " x ", Tapia, " = ", Kim * Tapia)
